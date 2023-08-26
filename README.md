fc4d1bf32ea490995e6b91b54712a60e2755eeab
Stanislav-i <itandeducationua@gmail.com>
2023-08-26 22:54:48 +0300

    start

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # node-1
+
+https://drive.google.com/file/d/1Enjg00yNRc2jNPIYqyPx7I6CFxvfTYZV/view?usp=sharing
+
+https://drive.google.com/file/d/1WsC04dm2XHECpEXiuh-1XACgIMdRS7r6/view?usp=sharing
+
+https://drive.google.com/file/d/1ubMQjWgcEbCwmi2bfRqx1UDcxuCcegix/view?usp=sharing
+
+https://drive.google.com/file/d/1L5YP8Ln2SnNDvfAno1ntvMsy4umJBeRV/view?usp=sharing
---
+++ contacts.js
@@ -0,0 +1,45 @@
+import fs from "fs/promises";
+import { nanoid } from "nanoid";
+import path from "path";
+
+const contactsPath = path.resolve("db", "contacts.json");
+const updateContacts = (contacts) =>
+  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const result = contacts.find((contact) => contact.id === contactId);
+  return result || null;
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await updateContacts(contacts);
+  return result;
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+
+  contacts.push(newContact);
+  await updateContacts(contacts);
+  return newContact;
+}
+
+export default { listContacts, getContactById, removeContact, addContact };
